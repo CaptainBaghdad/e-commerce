ea1421db72501bd4e95b64ca6d9b1ef9c55c3f41
me <juniorjr083@gmail.com>
2019-07-05 17:04:14 -0400

    added items to cart logic working

--- sales/models.py
+++ sales/models.py
@@ -25,11 +25,18 @@ class Item(models.Model):
     label = models.CharField(choices=LABEL_CHOICES, max_length=1)
     slug = models.SlugField()
     description = models.TextField()
+   
 
     def get_absolute_url(self):
         return reverse("products", kwargs={
             'slug': self.slug
         })
+    
+    def get_add_to_cart_url(self):
+         return reverse("add-to-cart", kwargs={
+            'slug': self.slug
+        })
+
     
     
     def __str__(self):
@@ -44,10 +51,12 @@ class Item(models.Model):
 
 
 class OrderedItem(models.Model):
+    ordered = models.BooleanField(default= False)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
-
+    quantity = models.IntegerField(default=1)
+    
     def __str__(self):
-        return self.title
+        return f"{self.quantity} of {self.item.title}"
 
 class Order(models.Model):
     user  = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
