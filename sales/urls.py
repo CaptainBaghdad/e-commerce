eba8d642f47f4704e4577248d14e42926cf93837
me <juniorjr083@gmail.com>
2019-07-08 09:58:52 -0400

    finished the checkout parge

--- sales/urls.py
+++ sales/urls.py
@@ -37,7 +37,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
     path('products/<slug>', ItemDetailView.as_view(),  name="products"),
-    path('checkout/', views.check_out),
+    #path('checkout/', views.check_out),
     path('add-to-cart/<slug>/', add_to_cart, name="add-to-cart"),
     path('remove-from-cart/<slug>/', remove_from_cart, name="remove-from-cart"),
     path('remove-item-from-cart/<slug>/', remove_single_item_from_cart,
